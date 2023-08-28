b5e773acb518f872d73299ae047d5787b5e24149
Sebastarazq <juansebastianhijomenor@hotmail.com>
2023-08-27 21:30:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,3 +6,16 @@ const app = express();
 // Habilitar pug
 app.set('view engine','pug') //usar pug
 app.set('views','./views') // aca estaran los archivos
+
+// Carpeta publica
+app.use(express.static('public'))
+
+
+//Routing
+app.use('/', appRoutes)
+
+//Definir un puerto y arrancar el proyecto // Para el correo process.env.PORT
+const port = 4000;
+app.listen(process.env.PORT || port, () =>{
+    console.log(`El servidor de ApoloWebApp esta funcionando en el puerto ${port}`)
+});
