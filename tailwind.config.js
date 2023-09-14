4941ac625df06abd7cf0279158ba17d565d57a47
Sebastarazq <juansebastianhijomenor@hotmail.com>
2023-09-13 23:04:34 -0500

    actulizacion

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./public/css/styles.css"], // Ruta al archivo de entrada de estilos
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
